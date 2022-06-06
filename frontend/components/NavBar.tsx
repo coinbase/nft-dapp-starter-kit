23f991c7549ff8159171589174cbe4c67a4e42d2
jeongmincho <jeongminc.jm@gmail.com>
2022-06-06 02:22:07 -0400

    ADDED: opensea icon

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -1,20 +1,27 @@
 import {
   Button,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerOverlay,
   IconButton,
-  Spacer,
-  Stack,
   useDisclosure,
+  Image,
+  Box,
 } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import Link from "next/link";
 import { FaDiscord, FaTwitter } from "react-icons/fa";
 import styles from "../styles/Navbar.module.css";
 import ConnectWallet from "./web3/ConnectWallet";
+
+const FaOpensea = () => (
+  <Box
+    width="48px"
+    height="48px"
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <Image width="18px" height="18px" src="assets/navbar/opensea.png" />
+  </Box>
+);
 
 const NavBar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -62,16 +69,17 @@ const NavBar = () => {
             </Button>
           </a>
           <ConnectWallet size="md" />
+          <FaOpensea />
           <IconButton
             aria-label="twitter icon"
-            size="md"
+            size="lg"
             colorScheme="white"
             variant="ghost"
             icon={<FaTwitter />}
           />
           <IconButton
             aria-label="discord icon"
-            size="md"
+            size="lg"
             colorScheme="white"
             variant="ghost"
             icon={<FaDiscord />}
@@ -85,68 +93,6 @@ const NavBar = () => {
             onClick={onOpen}
           />
         </div>
-        <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
-          <DrawerOverlay />
-          <DrawerContent background="black">
-            <DrawerCloseButton />
-            <DrawerBody onClick={onClose}>
-              <Stack marginTop="20" spacing="24px">
-                <Link href="/" passHref>
-                  <button className={styles.button}>Home</button>
-                </Link>
-                <Link href="/#about" passHref>
-                  <button className={styles.button}>About</button>
-                </Link>
-                <Link href="/#team" passHref>
-                  <button className={styles.button}>Team</button>
-                </Link>
-                <Link href="/#roadmap" passHref>
-                  <button className={styles.button}>Roadmap</button>
-                </Link>
-                <Link href="/#faq" passHref>
-                  <button className={styles.button}>FAQ</button>
-                </Link>
-                <Link href="/viewer" passHref>
-                  <button className={styles.button}>Explorer</button>
-                </Link>
-                <Spacer />
-                <a
-                  href="https://github.com/CoinbaseWallet/nft-minting-starter-kit"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <Button
-                    style={{
-                      fontFamily: "'Press Start 2P', cursive",
-                      color: "#FFFFFF",
-                      borderRadius: "0",
-                    }}
-                    colorScheme="whiteAlpha"
-                    variant="ghost"
-                    size="xs"
-                  >
-                    Source code
-                  </Button>
-                </a>
-                <ConnectWallet size="xs" />
-                <IconButton
-                  aria-label="twitter icon"
-                  size="md"
-                  colorScheme="whiteAlpha"
-                  variant="ghost"
-                  icon={<FaTwitter />}
-                />
-                <IconButton
-                  aria-label="discord icon"
-                  size="md"
-                  colorScheme="whiteAlpha"
-                  variant="ghost"
-                  icon={<FaDiscord />}
-                />
-              </Stack>
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
       </div>
     </div>
   );
