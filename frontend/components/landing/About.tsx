cc037f455441b91e61ff406c92838005cbef5a8f
jeongmincho <jeongminc.jm@gmail.com>
2022-06-06 00:53:02 -0400

    UPDATED: about page writing

--- frontend/components/landing/About.tsx
+++ frontend/components/landing/About.tsx
@@ -8,10 +8,10 @@ const About = () => {
           <h1 className={styles.blueTitle}>About</h1>
           <div className={styles.content}>
             <p className={styles.description}>
-              {`What are coinbaes? No one knows. But that doesn't matter, because this project is all about them!`}
+              {`What are coinbaes? Coinbaes are 5,000 unique and randomly generated NFTs that live out beyond the solar system in the Cryptomeda Galaxy.`}
               <br />
               <br />
-              {`Non fungible Coinbaes is the perfect way to show your friends and family how much you love crypto. With this project, you can buy, sell, and trade virtual baes that represent nothing more than pixels on a screen. But that's not all! You can also use these tokens to buy virtual goods and services. So what are you waiting for? Mint a coinbae as an NFT.`}
+              {`Owning a non-fungible coinbaes is the perfect way to show your friends and family how much you love, live, and breath crypto. With this project, you can buy, sell, and trade virtual baes that represent nothing more than 32 x 32 pixels on a screen.  So what are you waiting for? Share some love by minting a Coinbae.`}
             </p>
           </div>
         </main>
