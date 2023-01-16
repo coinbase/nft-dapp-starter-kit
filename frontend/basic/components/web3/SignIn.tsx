8d054f70720e2d87050ce44abe7bd8aa5b52874f
henryjin3 <henry@cadenzadevelopment.com>
2023-01-16 16:50:29 +0800

    Actually check NFT

--- frontend/basic/components/web3/SignIn.tsx
+++ frontend/basic/components/web3/SignIn.tsx
@@ -120,50 +120,47 @@ export function Profile() {
     return () => window.removeEventListener('focus', handler);
   }, []);
 
-  if (accountData) {
-    return (
-      <div className={styles.background}>
-        <div className={styles.container}>
-          <main className={styles.main}>
-            <h1 className={styles.title}>asdf</h1>
-            <div>
-              {state.address ? (
-                <div>
-                  <div style={{ color: 'white' }}>
-                    Signed in as {state.address}
-                  </div>
-                  <Button
-                    style={{
-                      color: '#4b4f56',
-                      borderRadius: '0',
-                    }}
-                    onClick={async () => {
-                      await fetch('/api/logout');
-                      setState({});
-                    }}
-                  >
-                    Sign Out
-                  </Button>
-                </div>
-              ) : (
-                <SignInButton
-                  onSuccess={({ address }) =>
-                    setState((x) => ({ ...x, address }))
-                  }
-                  onError={({ error }) => setState((x) => ({ ...x, error }))}
-                />
-              )}
-            </div>
-          </main>
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <div>
-      {' '}
-      <p>fdsa</p>
+    <div className={styles.background}>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          {accountData ? (
+            <>
+              <h1 className={styles.title}>Sign in!</h1>
+              <div>
+                {state.address ? (
+                  <div>
+                    <div style={{ color: 'white' }}>
+                      Signed in as {state.address}
+                    </div>
+                    <Button
+                      style={{
+                        color: '#4b4f56',
+                        borderRadius: '0',
+                      }}
+                      onClick={async () => {
+                        await fetch('/api/logout');
+                        setState({});
+                      }}
+                    >
+                      Sign Out
+                    </Button>
+                  </div>
+                ) : (
+                  <SignInButton
+                    onSuccess={({ address }) =>
+                      setState((x) => ({ ...x, address }))
+                    }
+                    onError={({ error }) => setState((x) => ({ ...x, error }))}
+                  />
+                )}
+              </div>
+            </>
+          ) : (
+            <div style={{ color: 'white' }}>Connect your wallet first!</div>
+          )}{' '}
+        </main>
+      </div>
     </div>
   );
 }
--- frontend/basic/pages/api/verify.ts
+++ frontend/basic/pages/api/verify.ts
@@ -2,7 +2,39 @@ import { ironOptions } from 'global/ironOptions';
 import { withIronSessionApiRoute } from 'iron-session/next';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { SiweMessage } from 'siwe';
+import { ethers } from 'ethers';
+import { abi } from 'global/erc721';
 
+const baseUrl = process.env.NODE_ENDPOINT ?? '';
+const username = process.env.NODE_USERNAME;
+const password = process.env.NODE_PASSWORD;
+
+const validateNFTOwnership = async (address: string) => {
+  try {
+    const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ?? '';
+    // Create node provider using project credentials
+    const provider = new ethers.providers.JsonRpcProvider({
+      url: baseUrl,
+      user: username,
+      password: password,
+    });
+    const contract = await new ethers.Contract(
+      contractAddress,
+      abi(),
+      provider
+    );
+    const balance = await contract.balanceOf(address);
+    if (balance.toString() !== '0') {
+      console.debug(`Balance is ${balance}`);
+      return true;
+    }
+
+    return false;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req;
   switch (method) {
@@ -11,6 +43,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         const { message, signature } = req.body;
         const siweMessage = new SiweMessage(message);
         const fields = await siweMessage.validate(signature);
+        const ownsNFT = await validateNFTOwnership(siweMessage.address);
+        console.debug(`ownsNFT? :${ownsNFT}`);
+        if (!ownsNFT)
+          return res.status(422).json({ message: 'Does not own NFT' });
 
         if (fields.nonce !== req.session.nonce)
           return res.status(422).json({ message: 'Invalid nonce.' });
